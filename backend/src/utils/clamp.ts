4cea9b1d82a4fdd23292779abf802c7a80d2ea63
alexy4744 <alexy4744@gmail.com>
2020-08-12 14:37:23 -0400

    remove more unused utils

--- backend/src/utils/clamp.ts
+++
@@ -1,2 +0,0 @@
-export const clamp = (x: number, min: number, max: number): number =>
-  Math.min(Math.max(x, min), max);
--- backend/src/utils/delay.ts
+++
@@ -1,2 +0,0 @@
-export const delay = (time: number): Promise<void> =>
-  new Promise((resolve) => setTimeout(resolve, time));
