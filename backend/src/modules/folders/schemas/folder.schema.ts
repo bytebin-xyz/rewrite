1444e7fb795aa73925df7d46a1600972cbb39d19
alexy4744 <alexy4744@gmail.com>
2020-08-26 16:38:30 -0400

    refactor(files): remove population of parent field on pre find and post save of folder schema

--- backend/src/modules/folders/schemas/folder.schema.ts
+++ backend/src/modules/folders/schemas/folder.schema.ts
@@ -75,19 +75,10 @@ export class Folder extends Document implements FolderDto {
   })
   uid!: string;
 
-  populateParent!: () => Promise<Folder | null>;
   toDto!: () => FolderDto;
 }
 
 export const FolderSchema = SchemaFactory.createForClass(Folder);
-
-FolderSchema.pre<Folder>("find", function(this: Folder) {
-  this.populate("parent");
-});
-
-FolderSchema.pre<Folder>("findOne", function(this: Folder) {
-  this.populate("parent");
-});
 
 FolderSchema.pre<Folder>("save", function(next) {
   if (!this.isNew) return next();
@@ -104,7 +95,9 @@ FolderSchema.pre<Folder>("save", async function(next) {
   if (!this.isModified("name") && !this.isModified("parent")) return next();
 
   try {
-    const parent = await this.populateParent();
+    const parent = await this.populate("parent")
+      .execPopulate()
+      .then(folder => folder.parent as Folder | null);
 
     const newPath = parent ? `${parent.path}/${this.name}` : `/${this.name}`;
     const oldPath = this.path.toString();
@@ -119,28 +112,17 @@ FolderSchema.pre<Folder>("save", async function(next) {
         await child.save();
       });
 
+    this.depopulate("parent");
+
     next();
   } catch (error) {
     next(error);
   }
 });
 
-FolderSchema.post<Folder>("save", function(doc, next) {
-  doc
-    .populateParent()
-    .then(() => next())
-    .catch(error => next(error));
-});
-
 FolderSchema.virtual("deepness").get(function(this: Folder) {
   return this.path.split("/").filter(el => el.length > 0).length;
 });
-
-FolderSchema.methods.populateParent = async function(this: Folder): Promise<Folder | null> {
-  await this.populate("parent").execPopulate();
-
-  return this.parent as Folder | null;
-};
 
 FolderSchema.methods.toDto = function(this: Folder): FolderDto {
   return plainToClass(FolderDto, this.toJSON(), {
