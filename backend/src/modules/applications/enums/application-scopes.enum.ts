7a7336e100d572f42db79dfadffe914892d55485
alexy4744 <alexy4744@gmail.com>
2020-09-06 18:01:41 -0400

    refactor(applications): remove folders:read and folders:write scopes

--- backend/src/modules/applications/enums/application-scopes.enum.ts
+++ backend/src/modules/applications/enums/application-scopes.enum.ts
@@ -2,8 +2,5 @@ export enum ApplicationScopes {
   FILES_READ = "files:read",
   FILES_WRITE = "files:write",
 
-  FOLDERS_READ = "folders:read",
-  FOLDERS_WRITE = "folders:write",
-
   USERS_READ = "users:read"
 }
