7db95b476379193ecd7c470deb60132718039c87
alexy4744 <alexy4744@gmail.com>
2020-09-06 23:20:18 -0400

    feat(storage): add static message and status properties to errors

--- backend/src/modules/storage/storage.errors.ts
+++ backend/src/modules/storage/storage.errors.ts
@@ -1,37 +1,60 @@
-import { BadRequestException, PayloadTooLargeException, UnsupportedMediaTypeException } from "@nestjs/common";
+import {
+  BadRequestException,
+  HttpStatus,
+  PayloadTooLargeException,
+  UnsupportedMediaTypeException
+} from "@nestjs/common";
 
 export class FileTooLarge extends PayloadTooLargeException {
+  static message = `${FileTooLarge.name}: The file you tried to upload is too large!`;
+  static status = HttpStatus.PAYLOAD_TOO_LARGE;
+
   constructor(filename: string) {
-    super(`File '${filename}' is too large!`);
+    super(`${FileTooLarge.name}: File '${filename}' is too large!`);
   }
 }
 
 export class NoFilesUploaded extends BadRequestException {
+  static message = `${NoFilesUploaded.name}: No files were uploaded!`;
+  static status = HttpStatus.BAD_REQUEST;
+
   constructor() {
-    super("No files uploaded!");
+    super(NoFilesUploaded.message);
   }
 }
 
 export class TooManyFields extends BadRequestException {
+  static message = `${TooManyFields.name}: There were too many fields associated with your form!`;
+  static status = HttpStatus.BAD_REQUEST;
+
   constructor() {
-    super("Too many fields!");
+    super(TooManyFields.message);
   }
 }
 
 export class TooManyFiles extends BadRequestException {
+  static message = `${TooManyFiles.name}: The amount of files you attempted to upload exceeded the limit!`;
+  static status = HttpStatus.BAD_REQUEST;
+
   constructor() {
-    super("Too many files!");
+    super(TooManyFiles.message);
   }
 }
 
 export class TooManyParts extends BadRequestException {
+  static message = `${TooManyParts.name}: Too many parts!`;
+  static status = HttpStatus.BAD_REQUEST;
+
   constructor() {
-    super("Too many parts!");
+    super(TooManyParts.name);
   }
 }
 
 export class UnsupportedContentType extends UnsupportedMediaTypeException {
+  static message = `${TooManyParts.name}: Missing or unsupported content type!`;
+  static status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
+
   constructor() {
-    super("Missing or unsupported content type!")
+    super(UnsupportedContentType.message);
   }
 }
