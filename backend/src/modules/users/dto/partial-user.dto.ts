67faa2aec9de0ee8d14413178f793448b767848f
alexy4744 <alexy4744@gmail.com>
2020-09-12 17:15:53 -0400

    feat(users): add swagger description to user and partial user dto

--- backend/src/modules/users/dto/partial-user.dto.ts
+++ backend/src/modules/users/dto/partial-user.dto.ts
@@ -1,16 +1,35 @@
 import { ApiProperty } from "@nestjs/swagger";
 
 export class PartialUserDto {
+  @ApiProperty({
+    description: "Flag that indicates whether this user is an admin or not.",
+    example: false
+  })
   admin!: boolean;
 
-  @ApiProperty({ nullable: true, type: String })
+  @ApiProperty({
+    description:
+      "The file ID that points to the user's avatar. If the user has no avatar, null will be returned.",
+    nullable: true,
+    type: String
+  })
   avatar!: string | null;
 
+  @ApiProperty({
+    description: "The time of which this user created their account.",
+    example: new Date()
+  })
   createdAt!: Date;
 
+  @ApiProperty({ description: "The user's display name. This can differ from their username." })
   displayName!: string;
 
+  @ApiProperty({ description: "A unique identifier for this user." })
   id!: string;
 
+  @ApiProperty({
+    description:
+      "The username for this user. The username is always lowercase and cannot be changed."
+  })
   username!: string;
 }
--- backend/src/modules/users/dto/user.dto.ts
+++ backend/src/modules/users/dto/user.dto.ts
@@ -7,25 +7,41 @@ export class UserDto {
   @Exclude()
   activated!: boolean;
 
+  @ApiProperty({
+    description: "Flag that indicates whether this user is an admin or not.",
+    example: false
+  })
   admin!: boolean;
 
-  @ApiProperty({ nullable: true, type: String })
+  @ApiProperty({
+    description:
+      "The file ID that points to the user's avatar. If the user has no avatar, null will be returned.",
+    nullable: true,
+    type: String
+  })
   avatar!: string | null;
 
+  @ApiProperty({
+    description: "The time of which this user created their account.",
+    example: new Date()
+  })
   createdAt!: Date;
 
   @ApiHideProperty()
   @Exclude()
   deleted!: boolean;
 
+  @ApiProperty({ description: "The user's display name. This can differ from their username." })
   displayName!: string;
 
+  @ApiProperty({ description: "The email that was used to create this account." })
   email!: string;
 
   @ApiHideProperty()
   @Exclude()
   expiresAt!: Date | null;
 
+  @ApiProperty({ description: "A unique identifier for this user." })
   id!: string;
 
   @ApiHideProperty()
@@ -36,5 +52,9 @@ export class UserDto {
   @Exclude()
   updatedAt!: Date;
 
+  @ApiProperty({
+    description:
+      "The username for this user. The username is always lowercase and cannot be changed."
+  })
   username!: string;
 }
