c1bfc0e69232dfff678e8e413f276027e4c57c74
alexy4744 <alexy4744@gmail.com>
2020-09-12 17:35:39 -0400

    feat(storage): add static description getter for swagger in storage errors

--- backend/src/modules/storage/storage.errors.ts
+++ backend/src/modules/storage/storage.errors.ts
@@ -6,55 +6,79 @@ import {
 } from "@nestjs/common";
 
 export class FileTooLarge extends PayloadTooLargeException {
-  static message = `${FileTooLarge.name}: The file you tried to upload is too large!`;
+  static message = "The file(s) you tried to upload is too large!";
   static status = HttpStatus.PAYLOAD_TOO_LARGE;
 
   constructor(filename: string) {
     super(`${FileTooLarge.name}: File '${filename}' is too large!`);
   }
+
+  static get description(): string {
+    return `${FileTooLarge.name}: ${FileTooLarge.message}`;
+  }
 }
 
 export class NoFilesUploaded extends BadRequestException {
-  static message = `${NoFilesUploaded.name}: No files were uploaded!`;
+  static message = "You did not include any files to upload!";
   static status = HttpStatus.BAD_REQUEST;
 
   constructor() {
     super(NoFilesUploaded.message);
   }
+
+  static get description(): string {
+    return `${NoFilesUploaded.name}: ${NoFilesUploaded.message}`;
+  }
 }
 
 export class TooManyFields extends BadRequestException {
-  static message = `${TooManyFields.name}: There were too many fields associated with your form!`;
+  static message = "You have exceeded the maximum amont of fields per request!";
   static status = HttpStatus.BAD_REQUEST;
 
   constructor() {
     super(TooManyFields.message);
   }
+
+  static get description(): string {
+    return `${TooManyFields.name}: ${TooManyFields.message}`;
+  }
 }
 
 export class TooManyFiles extends BadRequestException {
-  static message = `${TooManyFiles.name}: The amount of files you attempted to upload exceeded the limit!`;
+  static message = "You have exceeded the maximum amount of files you can upload per request!";
   static status = HttpStatus.BAD_REQUEST;
 
   constructor() {
     super(TooManyFiles.message);
   }
+
+  static get description(): string {
+    return `${TooManyFiles.name}: ${TooManyFiles.message}`;
+  }
 }
 
 export class TooManyParts extends BadRequestException {
-  static message = `${TooManyParts.name}: Too many parts!`;
+  static message = "You have exceeded the maximum amount of parts per request!";
   static status = HttpStatus.BAD_REQUEST;
 
   constructor() {
     super(TooManyParts.name);
   }
+
+  static get description(): string {
+    return `${TooManyParts.name}: ${TooManyParts.message}`;
+  }
 }
 
 export class UnsupportedContentType extends UnsupportedMediaTypeException {
-  static message = `${UnsupportedContentType.name}: Missing or unsupported content type!`;
+  static message = "Missing or unsupported content type!";
   static status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
 
   constructor() {
     super(UnsupportedContentType.message);
+  }
+
+  static get description(): string {
+    return `${UnsupportedContentType.name}: ${UnsupportedContentType.message}`;
   }
 }
